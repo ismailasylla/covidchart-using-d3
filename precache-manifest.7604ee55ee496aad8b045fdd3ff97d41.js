7404ff25b5fd95c72061a6245e6fb749148de5c6
ismailasylla <ismailasyllaa@gmail.com>
2020-07-12 07:42:16 +0400

    Updates

---
+++ precache-manifest.7604ee55ee496aad8b045fdd3ff97d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df500ba065e66c6218f8685cab33bb6",
+    "url": "/covidchart-using-d3/index.html"
+  },
+  {
+    "revision": "730ad0bd80b57c785e03",
+    "url": "/covidchart-using-d3/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "617d5b672d4a37cf5eb2",
+    "url": "/covidchart-using-d3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "730ad0bd80b57c785e03",
+    "url": "/covidchart-using-d3/static/js/2.870a4d29.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/covidchart-using-d3/static/js/2.870a4d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617d5b672d4a37cf5eb2",
+    "url": "/covidchart-using-d3/static/js/main.620908d8.chunk.js"
+  },
+  {
+    "revision": "e2922355c58b9474682d",
+    "url": "/covidchart-using-d3/static/js/runtime-main.7d9e0c4e.js"
+  }
+]);
